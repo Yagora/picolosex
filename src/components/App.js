0c75d91defa6b249942321284382f20d4646f3b0
Benoit <benoit.chassignol@gmail.com>
2017-08-11 15:34:02 +0200

    [APP] Multiple page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 //node_modules
 import React, { Component } from 'react'
-//lib
-import { getActionBase, getQuestionBase, getBuildingFinish } from './../lib/baseAggregate'
 //other
 import './../styles/App.css'
 
@@ -15,14 +13,18 @@ class App extends Component {
             second_player_name: [false, 'Joueur 2'],
             second_player_clothes: [false, null],
             data_players: false,
+            timer: false,
+            question: false,
+            action: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.fetchData = this.fetchData.bind(this);
+        this.launchTimer = this.launchTimer.bind(this);
     }
 
     handleChange(event){
-        let stateToSet = event.target.name;
-        this.setState({[stateToSet]: [true, event.target.value]})
+        this.setState({[event.target.name]: [true, event.target.value]})
     }
 
     handleClick(){
@@ -34,12 +36,59 @@ class App extends Component {
             }
     }
 
+    fetchData(){
+        this.setState({timer : [this.getTimer()]});
+        this.setState({action : [this.getAction()]});
+        this.setState({question : [this.getQuestion()]});
+    }
+
+    launchTimer() {
+        setInterval(this.countDown(), 1000);
+     }
+
+    countDown(){
+        console.log(this.state.timer)
+        let seconds = this.state.timer - 1;
+        this.setState({
+            timer: seconds
+        });
+
+        if (seconds == 0) {
+            clearInterval(this.timer);
+        }
+    }
+
+    getTimer(){
+        return 30
+    }
+
+    getQuestion(){
+        return this.state.first_player_name[1] + " doit dire la couleur de préférée de " + this.state.second_player_name[1] + " sinon il boit"
+    }
+
+    getAction(){
+        return this.state.first_player_name[1] + " embrasse " + this.state.second_player_name[1] + " pendant 30 seconde"
+    }
+
   render() {
     if(this.state.data_players){
         return(
             <div className="App">
-                <div className="timer">Ici il y aura un super timer</div>
-                <div className="question">Ici la question</div>
+                {this.state.timer ?
+                    <div className="game-wrapper">
+                        <div className="timer">{this.state.timer}</div>
+                        <input type="button" value="launch timer" onClick={this.launchTimer}/>
+                        {this.state.question ?
+                            <div className="question">{this.state.question}</div>
+                            : <div className="action">{this.state.action[1]}</div>
+                        }
+                    </div>
+                :
+                    <div className="launche-wrapper">
+                        Si vous êtes prêt à baiser comme des chacals lancez le jeu bande de chacal ! <br />
+                        <input type="button" value="BAISER BAISER BAISER" onClick={this.fetchData}/>
+                    </div>
+                }
             </div>
         )
     } else {
@@ -48,7 +97,7 @@ class App extends Component {
               <form>
                   <div className="first_player">
                       <h1>{this.state.first_player_name}</h1>
-                      <input onChange={this.handleChange} name="first_player_name" type="text" required="required" placeholder={this.state.first_player_name}/>
+                      <input onChange={this.handleChange} name="first_player_name" type="text" required="required" placeholder="Pseudo"/>
                       <input onChange={this.handleChange} name="first_player_clothes" type="number" required="required" placeholder="Nombres de vêtements"/>
                   </div>
                   <div className="second_player">
@@ -56,7 +105,7 @@ class App extends Component {
                       <input onChange={this.handleChange} name="second_player_name" type="text" required="required" placeholder="Pseudo"/>
                       <input onChange={this.handleChange} name="second_player_clothes" type="number" required="required" placeholder="Nombres de vêtements"/>
                   </div>
-                  <input type="button" value="GO !" onClick={this.handleClick}/>
+                  <input type="button" value="Valider" onClick={this.handleClick}/>
               </form>
           </div>
         )
