0e656ca4a715fb57f83162f101625238d07ffb29
Tanguy Charon <tcharon@arkoon.dev>
2019-02-04 14:42:17 +0100

    Framework change

--- src/class/Action.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Action {
-	constructor(object) {
-		this.question = object.action;
-		this.timer = object.timer;
-		this.force = object.force;
-		this.naked = object.naked || false;
-	}
-};
--- src/class/Question.js
+++
@@ -1,6 +0,0 @@
-module.exports = class Question {
-	constructor(object) {
-		this.question = object.question;
-		this.force = object.force;
-	}
-};
